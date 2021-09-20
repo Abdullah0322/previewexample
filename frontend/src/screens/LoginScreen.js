e68cb6cd7d1220f4601feadac28042805d0dd80c
Rehan Munir <rehanir.munir@gmail.com>
2021-09-20 19:06:19 +0500

    changes

--- frontend/src/screens/LoginScreen.js
+++ frontend/src/screens/LoginScreen.js
@@ -43,6 +43,12 @@ setUser({
 
 }
   return (
+ <Helmet>
+    <meta id="meta-description" name="description" content="Some description." />
+    <meta id="og-title" property="og:title" content="Home Page" />
+    <meta id="og-image" property="og:image" content="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1200px-Image_created_with_a_mobile_phone.png"/>
+  </Helmet>
+    
    
     <FormContainer>
       <h1>Sign In</h1>
